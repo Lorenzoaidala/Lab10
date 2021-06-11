a57b39afc055ec58249c85cd928108b5e54c616d
Lorenzo Aidala <Lorenzo Aidala@DESKTOP-21723CJ.homenet.telecomitalia.it>
2021-06-11 13:10:09 +0200

    scrittura metodi del DAO in corso

--- src/main/java/it/polito/tdp/rivers/db/RiversDAO.java
+++ src/main/java/it/polito/tdp/rivers/db/RiversDAO.java
@@ -3,6 +3,7 @@ package it.polito.tdp.rivers.db;
 import java.util.LinkedList;
 import java.util.List;
 
+import it.polito.tdp.rivers.model.Flow;
 import it.polito.tdp.rivers.model.River;
 
 import java.sql.Connection;
@@ -12,11 +13,11 @@ import java.sql.SQLException;
 
 public class RiversDAO {
 
-	public List<River> getAllRivers() {
+	public void getAllRivers(Map<Integer,River> idMap) {
 		
 		final String sql = "SELECT id, name FROM river";
 
-		List<River> rivers = new LinkedList<River>();
+		//List<River> rivers = new LinkedList<River>();
 
 		try {
 			Connection conn = DBConnect.getConnection();
@@ -24,7 +25,7 @@ public class RiversDAO {
 			ResultSet res = st.executeQuery();
 
 			while (res.next()) {
-				rivers.add(new River(res.getInt("id"), res.getString("name")));
+				idMap.put(res.getInt(("id"), rs.get)
 			}
 
 			conn.close();
@@ -35,5 +36,47 @@ public class RiversDAO {
 		}
 
 		return rivers;
+	}
+	
+	public double getAvgFlow(int river_id) {
+		
+		String sql="SELECT AVG(flow)AS media "
+				+ "FROM flow "
+				+ "WHERE river =?";
+		
+		
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, river_id);
+			ResultSet res = st.executeQuery();
+			double result = res.getDouble("media");
+			return result;
+		}catch(SQLException e) {
+			throw new RuntimeException("Error Connection Database");
+		}
+	}
+	
+	public List<Flow> getFlowsForRIver(int river_id){
+		String sql ="SELECT DAY,flow "
+				+ "FROM flow "
+				+ "WHERE river =?";
+		
+		List<Flow> result = new LinkedList<Flow>();
+		
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, river_id);
+			ResultSet res = st.executeQuery();
+			
+			while(res.next()) {
+				result.add(new Flow(res.get("DAY"), res.getDouble("flow"),river_id ));
+			}
+			
+			
+		}catch(SQLException e) {
+			throw new RuntimeException("Error Connection Database");
+		}
 	}
 }
--- src/main/java/it/polito/tdp/rivers/model/Model.java
+++ src/main/java/it/polito/tdp/rivers/model/Model.java
@@ -1,5 +1,20 @@
 package it.polito.tdp.rivers.model;
 
-public class Model {
+import it.polito.tdp.rivers.db.RiversDAO;
+import java.util.*;
 
+public class Model {
+	
+	
+	RiversDAO dao;
+	
+	public Model() {
+		dao = new RiversDAO();
+	}
+
+	public List<River> getAllRivers(){
+		return dao.getAllRivers();
+	}
+	
+	public 
 }
